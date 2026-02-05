86643123469209a0b4eafe1a5f6a404af56979e2
dardaganidzek-sudo <dardaganidzek@gmail.com>
2026-02-05 20:13:26 +0400

    first commit

---
+++ day 00/homework/main.py
@@ -0,0 +1,59 @@
+from turtle import*
+
+
+#we want to paint a house
+
+#step 1: draw a square
+shape("turtle")
+#speed (30)
+width(7)
+color("pink")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of square
+
+#drawing a door
+
+forward(70)
+color("purple")
+left(90)
+forward(120)    #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+
+penup()
+goto(200, 200)
+pendown()
+
+color("black")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+exitonclick()
+
+
+
+
+name = "kato"
+age = 14
+lastname = "dardaganidze"
+
+
+print(name)
+print(age)
+print(lastname)
